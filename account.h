9edbc8433c4425de100ff4fc3d4a97faafc85c89
aviranhazout <aviranhazout@campus.technion.ac.il>
2018-12-11 16:14:33 +0200

    needed files

---
+++ account.h
@@ -0,0 +1,8 @@
+//
+// Created by user on 11/12/18.
+//
+
+#ifndef HW2_ACCOUNT_H
+#define HW2_ACCOUNT_H
+
+#endif //HW2_ACCOUNT_H
---
+++ defines.h
@@ -0,0 +1,8 @@
+//
+// Created by user on 11/12/18.
+//
+
+#ifndef HW2_DEFINES_H
+#define HW2_DEFINES_H
+
+#endif //HW2_DEFINES_H
